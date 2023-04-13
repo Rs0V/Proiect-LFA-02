b9a35de8b71b5ad5456a29d5deb8613db97fcdd3
Rs0V <--->
2023-04-14 01:54:26 +0300

    Programul poate procesa si λ-NFA-uri acum.

--- Proiect-LFA-02/Proiect-LFA-02.cpp
+++ Proiect-LFA-02/Proiect-LFA-02.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <map>
 
+//#include <io.h>
+//#include <fcntl.h>
+
 #define PRINT_SWITCH 0
 #define STR std::string
 #define STR_LIST std::vector<STR>
@@ -38,32 +41,97 @@ int str2int(const STR& str) {
 	return cast ? number : -1;
 }
 
+bool isLambdaNFA = false;
 STR_LIST split_string(const STR& str) {
 	STR_LIST list;
 	list.push_back("");
-	for (char c : str)
-		if (c == ' ')
-			list.push_back("");
+	for (char c : str) {
+		if (c == ' ') {
+			if (list.back().empty()) {
+				list.back().push_back(c);
+				isLambdaNFA = true;
+			}
+			else
+				list.push_back("");
+		}
 		else
 			list.back().push_back(c);
+	}
 	return list;
 }
 
+STR str_replace(const STR& str, const STR& find) {
+	STR new_str = "";
+	size_t li = 0;
+	size_t i = str.find(find, li);
+	while (i != std::string::npos) {
+		new_str += str.substr(li, i - li);
+		li = i + find.size();
+		i = str.find(find, li);
+	}
+	new_str += str.substr(li, str.size() - li);
+	return new_str;
+}
+
+STR list2str(const STR_LIST& list) {
+	STR new_str = "";
+	for (STR str : list) {
+		new_str += str;
+	}
+	return new_str;
+}
+
+bool printedLambda = false;
 void print_all_nwords(int nr_letters, STR& curr_state, STR_LIST& end_states, DICT& dict, STR_LIST& path) {
 	if (nr_letters == 0) {
+		bool hasLambdaTrans = false;
+
+		for (auto& letter_map : dict[curr_state]) {
+			if (letter_map.first == "lambda" || letter_map.first == " ") {
+				hasLambdaTrans = true;
+				break;
+			}
+		}
+
 		for (STR end_state : end_states) {
 			if (curr_state == end_state) {
-				std::cout << path << "\n";
-				break;
+				if (path.empty() && !printedLambda) {
+					std::cout << "cuvantul vid" << '\n';
+					printedLambda = true;
+				}
+				else
+					std::cout << path << "\n";
+				return;
+			}
+		}
+
+		if (hasLambdaTrans) {
+			for (auto& letter_map : dict[curr_state]) {
+				bool isletterLambda = (letter_map.first == "lambda" || letter_map.first == " ");
+
+				for (auto& state : letter_map.second) {
+					if (isletterLambda)
+						print_all_nwords(nr_letters, state, end_states, dict, path);
+				}
 			}
 		}
 	}
 	else {
 		for (auto& letter_map : dict[curr_state]) {
-			path.push_back(letter_map.first);
-			for (auto& state : letter_map.second)
-				print_all_nwords(nr_letters - 1, state, end_states, dict, path);
-			path.pop_back();
+			bool isletterLambda = (letter_map.first == "lambda" || letter_map.first == " ");
+
+			if (!isletterLambda)
+				path.push_back(letter_map.first);
+
+			for (auto& state : letter_map.second) {
+				if (!isletterLambda)
+					print_all_nwords(nr_letters - 1, state, end_states, dict, path);
+				else
+					print_all_nwords(nr_letters, state, end_states, dict, path);
+			}
+
+			if (!isletterLambda)
+				path.pop_back();
 		}
 	}
 }
@@ -84,9 +152,13 @@ int main() {
 	input_lines.pop_back();
 	DICT dict;
 
+	bool lambdaOnly = true;
 	for (int i = 0; i < input_lines.size() - 1; ++i) {
 		STR_LIST vstr = split_string(input_lines[i]);
 		auto state_iter = dict.find(vstr[0]);
+
+		if (vstr[1] != "lambda" && vstr[1] != " ")
+			lambdaOnly = false;
 
 		if (state_iter != dict.end()) {
 			auto letter_iter = state_iter->second.find(vstr[1]);
@@ -116,8 +188,16 @@ int main() {
 	end_states = split_string(input_lines.back());
 	STR_LIST path;
 
-	for (int i = 1; i <= n; ++i)
-		print_all_nwords(i, begin_state, end_states, dict, path);
+	if (lambdaOnly)
+		std::cout << "cuvantul vid";
+	else {
+		for (int i = 1 - isLambdaNFA; i <= n; ++i)
+			print_all_nwords(i, begin_state, end_states, dict, path);
+	}
+
+	// PRINTING WITH LAMBDA UNICODE SYMBOL (JUST A TEST)
+	//_setmode(_fileno(stdout), _O_U16TEXT);
+	//std::wcout << L"Hello, \u03bb!\n";
 
 	return 0;
 }
